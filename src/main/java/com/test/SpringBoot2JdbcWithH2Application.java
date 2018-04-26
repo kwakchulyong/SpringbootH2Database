b20bcd8b41222fcc9d952db14d4751c61af84b43
Joseph Kwak <kwakchulyong@gmail.com>
2018-04-26 15:44:14 -0400

    It is first time update
    This board's function is only insert and select

---
+++ src/main/java/com/test/SpringBoot2JdbcWithH2Application.java
@@ -0,0 +1,31 @@
+package com.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.test.member.Member;
+import com.test.member.MemberJdbcRepository;
+
+@SpringBootApplication
+public class SpringBoot2JdbcWithH2Application implements CommandLineRunner {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	MemberJdbcRepository repository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2JdbcWithH2Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		logger.info("Student id 10001 -> {}", repository.findAll());
+
+	}
+}
