d7f4b1e3dc30022603adb5b1c0b3de8ceaf761b1
Joseph <Joseph@192.168.2.13>
2018-04-26 16:35:37 -0400

    run tomcat

--- src/main/java/com/test/SpringBoot2JdbcWithH2Application.java
+++ src/main/java/com/test/SpringBoot2JdbcWithH2Application.java
@@ -6,17 +6,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import com.test.member.Member;
 import com.test.member.MemberJdbcRepository;
 
 @SpringBootApplication
-public class SpringBoot2JdbcWithH2Application implements CommandLineRunner {
+public class SpringBoot2JdbcWithH2Application extends SpringBootServletInitializer implements CommandLineRunner {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	MemberJdbcRepository repository;
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringBoot2JdbcWithH2Application.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBoot2JdbcWithH2Application.class, args);
